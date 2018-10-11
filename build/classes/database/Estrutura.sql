7089e2aa29345cdbf7aeece921a116162486caf7
Alunos <gersonsbeltrame@gmail.com>
2018-10-11 09:28:48 -0300

    Initial commit

---
+++ build/classes/database/Estrutura.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS exemplo_hibernate_00;
+CREATE DATABASE exemplo_hibernate_00;
+USE exemplo_hibernate_00;
+
+CREATE TABLE istemas(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(150)
+);
